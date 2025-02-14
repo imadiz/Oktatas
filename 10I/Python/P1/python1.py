cec3c9e481d74ab5d77be77dd05fa362e98b114f
imadiz <weszerle42@gmail.com>
2025-02-14 13:04:12 +0100

    Új mappa, új P1

---
+++ 10I/Python/P1/python1.py
@@ -0,0 +1,214 @@
+#---------------------------------------------------------
+
+def osszead(szam1, szam2):
+    """ A függvény két számot kap és 
+        visszatér a két szám összegével.
+        
+        Nem használhatod a sum függvényt!
+    """
+    ...
+
+
+#---------------------------------------------------------
+
+def kisebb(szam1, szam2):
+    """ A függvény két számot kap és 
+        visszatér a kisebbel.
+        
+        Nem használhatod a min függvényt
+    """
+    ...
+
+
+#---------------------------------------------------------
+
+def nagyobb(szam1, szam2):
+    """ A függvény két számot kap és 
+        visszatér a nagyobbal.
+        
+        Nem használhatod a max függvényt.
+    """
+    ...
+
+
+#---------------------------------------------------------
+
+def szamtani_kozep(szam1, szam2):
+    """ A függvény két számot kap és 
+        visszatér a számtani középpel.
+    """
+    ...
+
+
+#---------------------------------------------------------
+ 
+def negyzet_kerulet(oldal):
+    """ A függvény egy négyzet oldalhosszát kapja bemenetként és 
+        visszatér a négyzet kerületével.
+    """
+    ...
+
+
+#---------------------------------------------------------
+ 
+def negyzet_terulet(oldal):
+    """ A függvény egy négyzet oldalhosszát kapja bemenetként és 
+        visszatér a négyzet területével.
+    """    
+    ...
+
+
+#---------------------------------------------------------
+ 
+def teglalap_kerulet(oldal1, oldal2):
+    """ A függvény egy téglalap oldalhosszait kapja bemenetként és 
+        visszatér a téglalap kerületével.
+    """
+    ...
+
+
+#---------------------------------------------------------
+ 
+def teglalap_terulet(oldal1, oldal2):
+    """ A függvény egy téglalap oldalhosszait kapja bemenetként és 
+        visszatér a téglalap területével.
+    """
+    ...
+
+
+#---------------------------------------------------------
+ 
+def kulonbseg(szam1, szam2):
+    """ A függvény két számot kap bemenetként és 
+        visszatér a két szám különbségével.
+    """    
+    ...
+
+
+#---------------------------------------------------------     
+
+def maradek(szam1, szam2):
+    """ A függvény két számot kap bemenetként és 
+        visszatér a két szám osztásának maradékával.
+    """    
+    ...
+
+
+#---------------------------------------------------------      
+
+def paros(szam):
+    """ A függvény egy számot kap bemenetként, 
+        majd True-val tér vissza, ha a szám páros és 
+        False-al ha a szám páratlan.
+    """    
+    ...
+
+
+#---------------------------------------------------------
+
+def kettovel_oszthato(szam):
+    """ A függvény egy számot kap bemenetként és 
+        visszatér True-val, ha a szám kettővel osztható és 
+        visszatér False-al ha a szám kettővel nem ossztható.
+    """    
+    ...
+
+
+#---------------------------------------------------------    
+def harommal_oszthato(szam):
+    """ A függvény egy számot kap bemenetként és 
+        True-val tér vissza, ha a szám hárommal osztható és 
+        False-al ha nem.
+    """    
+    ...
+
+
+#---------------------------------------------------------
+
+def hettel_oszthato(szam):
+    """ A függvény egy számot kap bemenetként és 
+        True-val tér vissza, ha a szám héttel osztható és 
+        False-al ha nem.
+    """    
+    ...
+
+
+#---------------------------------------------------------
+
+def kocka_terfogat(oldal):
+    """ A függvény bemenetként megkapja a kocka oldal hosszúságát és 
+        a kocka térfogatával tér vissza.
+    """    
+    ...
+
+
+#---------------------------------------------------------
+def teglatest_terfogat(a, b, c):
+    """ A függvény bemenetként megkapja a téglatest oldalainak hosszúságát és 
+        a téglatest térfogatával tér vissza.
+    """    
+    ...
+
+
+#---------------------------------------------------------
+
+def derekszogu_haromszog_terulet(befogo1, befogo2):
+    """ A függvény bemenetként megkapja a befogók hosszát és 
+        a háromszög területével tér vissza.
+    """    
+    ...
+
+
+#---------------------------------------------------------
+def derekszogu_haromszog_atfogo(befogo1, befogo2):
+    """ A függvény bemenetként megkapja a befogók hosszát és 
+        az átfogó hosszával tér vissza.
+    """    
+    ...
+
+#---------------------------------------------------------
+
+def negyzet_atloja(oldal):
+    """ A függvény bemenetként megkapja a négyzet oldalának hosszát és 
+        az átló hosszával tér vissza.
+    """    
+    ...
+
+
+#---------------------------------------------------------
+
+def teglalap_atloja(a, b):
+    """ A függvény bemenetként megkapja az oldalak hosszát és 
+        az átló hosszával tér vissza.
+    """    
+    ...
+
+
+#---------------------------------------------------------
+
+def abszolut(szam):
+    """ A függvény a bemenő paraméterként kapott szám 
+        abszolút értékével tér vissza.
+    """    
+    ...
+
+
+#---------------------------------------------------------
+
+def kor_kerulete(sugar):
+    """ A függvény a bemenő paraméterként kapja 
+        a kör sugarát és visszatér a kör kerületével.
+    """    
+    ...
+
+#---------------------------------------------------------
+
+def kor_terulete(sugar):
+    """ A függvény a bemenő paraméterként kapja 
+        a kör sugarát és visszatér a kör területével.
+    """    
+    ...   
+
+    
+#==========================================================
+
